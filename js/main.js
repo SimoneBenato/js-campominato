5ac7ebfd7499af75a65c0547fceb1599f85e64d0
SimoneBenato <sb270298@gmail.com>
2021-07-27 16:38:44 +0200

    Copiato i proc. grafici visti e ottim. cod logica

--- js/main.js
+++ js/main.js
@@ -1,43 +1,38 @@
 // Consegna
 // Il computer deve generare 16 numeri casuali (le nostre bombe) tra 1 e 100.
 // I numeri non possono essere duplicati.
+
+// variabili e array
 var bombs = [];
-while(bombs.length < 16){
-    var b = Math.floor(Math.random() * 100) + 1;
-    if(bombs.indexOf(b) === -1) bombs.push(b);
+var numeroElementi = 100;
+var numBombs = 16;
+
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 => tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
+
+
+
+// generatore delle bombe
+while(bombs.length < numBombs){
+    var numeroBomba = randomizer(numeroElementi);
+    if(bombs.indexOf(numeroBomba) === -1) {
+      bombs.push(numeroBomba);
+    } 
 }
 console.log(bombs);
 
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-var userNumbers = [];
-var rounds = 84;
-var haiPerso = false;
 
-while (haiPerso == false && userNumbers.length < rounds) {
-    var shot = parseInt(prompt("inserisci un numero da 1 a 100"));
-    // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-    if (isInArray(bombs, shot)) {
-      alert("KA-BOOM");
-      haiPerso = true;
-    } else {
-    // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-      if (isInArray(userNumbers, shot)) {
-        // L’utente non può inserire più volte lo stesso numero.
-        alert("numero già inserito")
-      } else {
-        userNumbers.push(shot);
-      }
-    }
-}
+var userNumbers = [];
+var rounds = numeroElementi - numBombs;
+
 
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-if (haiPerso == true) {
-    alert("Che peccato! Hai totalizato: " + userNumbers.length + " " + "punti");
-    console.log("Che peccato! Hai totalizato: " + userNumbers.length + " " + "punti");
-} else {
-    alert("Hai spaccato! E hai totalizato: " + userNumbers.length + " " + "punti");
-    console.log("Hai spaccato! E hai totalizato: " + userNumbers.length + " " + "punti");
-}
+
+
+// FUNZIONI
 
 function isInArray(array, element) {
   var i = 0;
@@ -52,18 +47,45 @@ function isInArray(array, element) {
   return result
 }
 
+function randomizer(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
 
-// BONUS: (da fare solo se funziona tutto il resto)
-// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
-// con difficoltà 0 => tra 1 e 100
-// con difficoltà 1 => tra 1 e 80
-// con difficoltà 2 => tra 1 e 50
-// Consigli del giorno:
-// Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-// Ad esempio:
-// Di cosa ho bisogno per generare i numeri?
-// Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-// Proviamo prima con pochi numeri, inserire 86 numeri ogni volta potrebbe essere un po’ scocciante
-// Le validazioni e i controlli possiamo farli anche in un secondo momento.
-// Ricordatevi che se non sappiamo quante volte dobbiamo fare una cosa ci serve…
-// E bon travail a tutti quanti. Vi ricordo che sono disponibile per i ticket fino alle 13
+
+// PARTE GRAFICA
+
+function creaCampo(celle) {
+  for (let i = 1; i <= celle; i++) {
+    let cella = `
+      <div data-cella="${i}" class="cella"></div>
+    `;
+    let templateCella = document.createElement('div');
+    templateCella.classList.add("quadrato");
+    templateCella.innerHTML = cella;
+    document.getElementById('campo').appendChild(templateCella);
+  }
+}
+
+document.getElementById('campo').addEventListener('click',
+    (e) => { //e = event
+    console.log(e.target.dataset.cella);
+    let element = document.querySelectorAll("[data-cella='" + e.target.dataset.cella + "']");
+    console.log(element[0]);
+    if (isInArray(bombs, e.target.dataset.cella)) {
+    element[0].classList.add("red");
+    alert("KA-BOOM");
+    alert("Che peccato! Hai totalizato: " + userNumbers.length + " " + "punti");
+    console.log("Che peccato! Hai totalizato: " + userNumbers.length + " " + "punti");
+    } else {
+        element[0].classList.add("green");
+        if (isInArray(userNumbers, e.target.dataset.cella)) {
+            // L’utente non può inserire più volte lo stesso numero.
+            alert("numero già inserito")
+        } else {
+            userNumbers.push(e.target.dataset.cella);
+        } 
+    }
+  }
+)
+
+creaCampo(100);
